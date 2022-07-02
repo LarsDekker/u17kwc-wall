acc036ba9df91cfb72eb18868e41b6441a310c48
LarsDekker <lars_dekker@live.nl>
2022-07-02 10:04:43 +0200

    Only planning

--- src/lib/Scores.svelte
+++ src/lib/Scores.svelte
@@ -8,7 +8,19 @@
         );
         const jsonData = await response.json();
 
-        return jsonData.value ?? [];
+        const date = new Date();
+        console.log(date.getDay());
+        if (date.getDay() === 6) {
+            return jsonData.value.slice(0, 25).filter((m) => {
+                console.log(m);
+                let hour = m.StartTime.split(":")[0];
+                let currentHour = date.getHours();
+                console.log(hour, currentHour);
+                return hour >= currentHour;
+            });
+        } else {
+            return jsonData.value.slice(25);
+        }
     };
 
     setInterval(() => {
@@ -17,37 +29,41 @@
 </script>
 
 <div class="score_content">
-    <div class="dflex p-2">
+    <div class="dflex p-2 h-20">
         <img src={logo} class="logo" alt="Svelte Logo" />
         <div class="title">
             <h1>Korfball World Cup</h1>
             <p>U17</p>
         </div>
     </div>
-    {#each matches as match}
-        <div class="match_wrapper">
-            <div class="dflex">
-                <h2 class="col-33">{match.TeamHome}</h2>
-                <span class="col-33">-</span>
-                <h2 class="col-33">{match.TeamAway}</h2>
-            </div>
-            <div class="dflex ">
-                <div class="score col-33">{match.GoalsHome}</div>
-                <div class="date col-33">
-                    {match.StartTime.substr(0, match.StartTime.length - 3)}
+    <div class="matches">
+        {#each matches as match}
+            <div class="match_wrapper">
+                <div class="dflex">
+                    <h2 class="col-33">{match.TeamHome}</h2>
+                    <span class="col-33">
+                        {match.StartTime.substr(0, match.StartTime.length - 3)}
+                    </span>
+                    <h2 class="col-33">{match.TeamAway}</h2>
                 </div>
-                <div class="score col-33">{match.GoalsAway}</div>
+                <!-- <div class="dflex ">
+                        <div class="score col-33">{match.GoalsHome}</div>
+                        <div class="date col-33">
+                            {match.StartTime.substr(0, match.StartTime.length - 3)}
+                        </div>
+                        <div class="score col-33">{match.GoalsAway}</div>
+                    </div> -->
             </div>
-        </div>
-    {/each}
+        {/each}
+    </div>
 </div>
 
 <style>
     .score_content {
         color: black;
-        padding: 40px;
+        padding: 0px 40px;
         height: 100%;
-        overflow: auto;
+        overflow: none;
         box-sizing: border-box;
 
         /* From https://css.glass */
@@ -58,9 +74,19 @@
         -webkit-backdrop-filter: blur(13.1px);
         border: 1px solid rgba(36, 142, 0, 1); */
     }
+    .h-20 {
+        height: 20%;
+    }
+
+    .matches {
+        height: 80%;
+        overflow: auto;
+        box-sizing: border-box;
+    }
 
     .p-2 {
         padding: 2rem;
+        height: 130px;
     }
 
     .logo {
@@ -86,13 +112,13 @@
         /* From https://css.glass */
         background: rgba(255, 255, 255, 0.83);
         border-radius: 16px;
-        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 2px 7px rgba(0, 0, 0, 0.2);
         backdrop-filter: blur(12.6px);
         -webkit-backdrop-filter: blur(12.6px);
         border: 1px solid rgb(238, 238, 238);
 
-        padding: 20px;
-        margin-bottom: 50px;
+        padding: 0px 0px 20px 0px;
+        margin-bottom: 10px;
     }
 
     .score {
